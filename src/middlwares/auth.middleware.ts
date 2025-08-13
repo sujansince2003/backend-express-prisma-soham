0d69c5a59c49059da506d73785363e42720979ef
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-08-13 08:05:00 +0545

    middleware function to verify cookie token

---
+++ src/middlwares/auth.middleware.ts
@@ -0,0 +1,43 @@
+import { NextFunction, Request, Response } from "express";
+
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+
+declare global {
+    namespace Express {
+        interface Request {
+            userId?: string
+        }
+    }
+}
+
+
+
+
+export default function verifyToken(req: Request, res: Response, next: NextFunction) {
+    const token = req.cookies.auth_token ||
+
+        req.header("Authorization")?.replace("Bearer ", "");
+
+    if (!token) {
+        res.status(401).json({
+            msg: "unathorized"
+        })
+        return;
+    }
+    try {
+
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET_KEY as string);
+        req.userId = (decodedToken as JwtPayload).userId;
+        next();
+
+    } catch (error) {
+        res.status(401).json({ msg: " unauthorized" });
+        return;
+    }
+
+
+
+
+
+}
